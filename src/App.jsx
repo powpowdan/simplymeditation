a1d416fb5d92af0206875d95d85ff3c775c7795f
powpowdan <powpowdan@gmail.com>
2024-11-24 20:31:27 -0500

    - removed screen sessionList.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import React from 'react'; 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
-import { MusicSwitchProvider } from './MusicSwitchContext';
-import { SessionProvider } from './SessionContext'; 
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {MusicSwitchProvider} from './MusicSwitchContext';
+import {SessionProvider} from './SessionContext';
 import OptionsScreen from './screens/OptionsScreen';
-import HomeScreen from './screens/HomeScreen'; 
-import SessionList from './SessionList';
-
-
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
@@ -17,40 +14,28 @@ const App = () => {
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
-              gestureEnabled: true, // Enable gestures for transitions (optional)
-              headerShown: false, // Hide header for all screens by default (optional)
+              gestureEnabled: true,
+              headerShown: false,
             }}>
-            {/* Meditation Timer Screen */}
+            {/* Home Screen */}
             <Stack.Screen
               name="Home"
               component={HomeScreen}
-              options={{headerShown: false,  animation: 'fade'}}
+              options={{animation: 'fade'}}
             />
             {/* Options Screen */}
             <Stack.Screen
-              name="Options" // Use "Options" as the screen name for OptionsScreen
+              name="Options"
               component={OptionsScreen}
               options={{
-                animation: 'fade',
-                title: 'Options',
-                headerStyle: {
-                  backgroundColor: '#212121', // Set the background color of the header
-                },
-                headerTintColor: '#ededed', // Set the text color of the header
-                headerTitleStyle: {
-                  fontWeight: 'bold',
-                },
+                animation: 'fade', 
               }}
             />
-            {/* Session List Screen */}
-            <Stack.Screen name="SessionList" component={SessionList} />
           </Stack.Navigator>
         </NavigationContainer>
       </SessionProvider>
     </MusicSwitchProvider>
   );
 };
-
-
 
 export default App;

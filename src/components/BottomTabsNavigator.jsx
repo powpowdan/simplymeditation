70a89315fb81d54c5362226c39fec6cc285856ba
powpowdan <powpowdan@gmail.com>
2024-12-30 14:45:45 -0500

    added vector icons to navigation, manual linking

--- src/components/BottomTabsNavigator.jsx
+++ src/components/BottomTabsNavigator.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StyleSheet, Animated} from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import OptionsScreen from '../screens/OptionsScreen';
 import StatsScreen from '../screens/StatsScreen';
-import { StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabsNavigator() {
   return (
     <Tab.Navigator
-      screenOptions={{
-        headerShown: false, // Hides the default header
-        tabBarStyle: styles.tabBar, // Style the tab bar
-        tabBarActiveTintColor: styles.activeTintColor.color, // Active icon color
-        tabBarInactiveTintColor: styles.inactiveTintColor.color, // Inactive icon color
-      }}
-    >
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarStyle: styles.tabBar,
+        // tabBarShowLabel: false,
+        tabBarActiveTintColor: styles.activeTintColor.color,
+        tabBarInactiveTintColor: styles.inactiveTintColor.color,
+        tabBarIcon: ({color, size, focused}) => {
+          let iconName;
+          let scale = focused ? 1.2 : 1;
+          if (route.name === 'Home') {
+            iconName = 'home-outline';
+          } else if (route.name === 'Statistics') {
+            iconName = 'stats-chart-outline';
+          } else if (route.name === 'Options') {
+            iconName = 'settings-outline';
+          }
+
+          return (
+            <Animated.View style={{transform: [{scale}]}}>
+              <Icon name={iconName} size={size} color={color} />
+            </Animated.View>
+          );
+        },
+        tabBarLabel: ({focused}) => {
+          return (
+            <Animated.Text
+              style={{
+                fontSize: focused ? 11 : 9, // Animate label size on focus
+                color: focused
+                  ? styles.activeTintColor.color
+                  : styles.inactiveTintColor.color,
+              }}>
+              {route.name}
+            </Animated.Text>
+          );
+        },
+        tabBarItemStyle: {
+          paddingBottom: 3,
+        },
+      })}>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Placeholder" component={StatsScreen} />
+      <Tab.Screen name="Statistics" component={StatsScreen} />
       <Tab.Screen name="Options" component={OptionsScreen} />
     </Tab.Navigator>
   );
@@ -27,6 +61,7 @@ export default function BottomTabsNavigator() {
 const styles = StyleSheet.create({
   tabBar: {
     backgroundColor: '#1A1F26',
+    height: '7%',
   },
   activeTintColor: {
     color: '#74aff7',

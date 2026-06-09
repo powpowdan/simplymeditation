8b6a5cdffa4b124611acb6243e7f3e3482cd34b1
powpowdan <powpowdan@gmail.com>
2026-06-09 09:22:25 -0400

    fix audio looping issue with phone asleep for OLDER phones killing audio pipeline. audio ref immediately called in callback

--- src/hooks/useMusic.js
+++ src/hooks/useMusic.js
@@ -1,6 +1,8 @@
 import { useState, useRef, useEffect } from 'react';
 import Sound from 'react-native-sound';
 import { useMusicSwitchContext } from '../context/MusicSwitchContext';
+// 1. Force Android to give this app high-priority background audio focus
+Sound.setCategory('Playback', true);
 
 const useMusic = () => {
   const { selectedSongPathBg, volumeBg } = useMusicSwitchContext();
@@ -16,15 +18,19 @@ const useMusic = () => {
         return;
       }
 
+      soundRef.current = sound; //NEW: Establish the reference immediately, trying loop fix
+
       sound.setVolume(volumeBg);
-      sound.setNumberOfLoops(-1); // 🔁 Native infinite looping
+      sound.setNumberOfLoops(-1); // Native infinite looping
+
       sound.play((success) => {
         if (!success) {
           console.error('Playback failed due to decoding errors');
+          // NEW : If it cuts out or stops unexpectedly, reset the state
+          setIsMusicPlaying(false);
         }
       });
-
-      soundRef.current = sound;
+ 
       setIsMusicPlaying(true);
     });
   };
